bc5723d1d95a07fd3a31fe49ab6c0ad80e24601d
S4ND1X <42609763+S4ND1X@users.noreply.github.com>
2019-06-05 21:13:15 -0500

    Codeforces Exercises

--- reverseStringParethesis.py
+++ reverseStringParethesis.py
@@ -1,20 +1,21 @@
-# string = "(abc)d(efg)"
-# def reverseInParentheses(s):
-#     n = s.count('(')
-#     for i in range(n):
-#         if '(' in s:
-#             start = s.find('(')
-#             end = s.find(')')
-#             new = s[start+1:end][::-1]
-#             s = s.replace(s[start:end+1],new)
-#     return s
+#reverse parent
+string = "(abc)d(efg)"
+def reverseInParentheses(s):
+    n = s.count('(')
+    for i in range(n):
+        if '(' in s:
+            start = s.find('(')
+            end = s.find(')')
+            new = s[start+1:end][::-1]
+            s = s.replace(s[start:end+1],new)
+    return s
 
-# print(reverseInParentheses(string))
+print(reverseInParentheses(string))
         
 
  
 
-
+#How to find domain of email
 
 
 import re
